44d6435ec12754689e9283259c7de57427eea834
hebaulf <heba.farestveit@gmail.com>
2022-03-23 10:08:45 +0000

    Add content to index page. Add form elements to personal info step. Remove footer style file.

--- components/Application/PersonalInfo.js
+++ components/Application/PersonalInfo.js
@@ -3,13 +3,54 @@ import TitleBlock from "../TitleBlock/TitleBlock";
 const PersonalInfo = () => {
   return (
     <div>
-      <TitleBlock 
-				title="Börn og foreldrar" 
-				text="Upplýsingar um foreldra/forsjáraðila koma frá Þjóðskrá og skólakerfum Reykjavíkurborgar. Athugaðu hvort símanúmer og netföng séu rétt áður en þú heldur áfram."
-			/>
-      <div className="form__section">
-        <h4>Börn</h4>
+      <div className="TextBlock">
+        <h2>Börn og foreldrar</h2>
+        <p>Upplýsingar um foreldra/forsjáraðila koma frá Þjóðskrá og skólakerfum Reykjavíkurborgar. Athugaðu hvort símanúmer og netföng séu rétt áður en þú heldur áfram.</p>
       </div>
+
+      <div className="FormField CheckboxButtonsGroup" role="group">
+        <h4 className="FormField__label" id="label:_6746485-124">Börn</h4>
+        <ul className="FormField__options" role="group" aria-labelledby="label:_6746485-124" aria-required="false" >
+          <li className="CheckboxButton FormField__options__item">
+            <input 
+              className="CheckboxButton__input" 
+              type="checkbox"
+              id="_6746485-125"
+              name="fruits"
+              value="Jón"
+            />
+            <label className="CheckboxButton__label" for="_6746485-125">
+              Rögnvaldur Björn Magnússon <small>030412-5479</small>
+            </label>
+          </li>
+          <li className="CheckboxButton FormField__options__item">
+            <input
+              className="CheckboxButton__input"
+              type="checkbox"
+              id="_6746485-126"
+              name="fruits"
+              value="Anna"
+              checked=""
+            />
+            <label className="CheckboxButton__label" for="_6746485-126">
+              Anna Ólöf Magnúsdóttir<small>130818-3237</small>
+            </label>
+          </li>
+        </ul>
+        <div className="Alert Alert--info Alert--closable" role="alert" hidden="">
+          Aðeins er hægt að sækja um í skólahljómsveit fyrir börn fædd fyrir 2014.
+        </div>
+      </div>
+
+      <fieldset class="FieldGroup" role="group">
+      <h4 className="FormField__label">Börn</h4>
+        <div class="FormField TextInput">
+          <label class="FormField__label" for="_6746485-193"> Pick your fruits</label>
+          <input class="FormField__input" type="text" id="_6746485-193" value="" />
+        </div>
+      </fieldset>
+      <br className="VSpacer VSpacer--small" />
+
       <div className="form__section">
         <h4>Foreldrar/forsjáraðilar</h4>
         <div className="form__infobox">
--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,142 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Home Page</title>
+        <title>Reykjavik - Mínar síður</title>
       </Head>
 
+      {/* <div class="HeroBlock">
+        <h1 class="HeroBlock__title">Mínar Síður</h1>
+        <picture class="HeroBlock__image">
+          <img
+            src="https://styles.reykjavik.is/assets/illustrations/minarsidur.png"
+            alt=""
+            loading="lazy"
+          />
+        </picture>
+        <div class="HeroBlock__summary">
+        Smelltu á Innskrá með island.is til að skrá þig inn á Mínar síður. Ekki hika við að hafa samband ef einhverjar spurningar vakna.
+        </div>
+        <div class="HeroBlock__buttons">
+          <button class="ButtonPrimary" type="button">Innskrá með island.is</button>
+          <a class="ButtonTertiary" href="/login">Innskrá með notendanafni</a>
+        </div>
+      </div> */}
+
+      <div class="HeroBlock">
+        <h1 class="HeroBlock__title">Hæ, Ragnheiður</h1>
+        <picture class="HeroBlock__image">
+          <img
+            src="https://styles.reykjavik.is/assets/illustrations/borgarstjori3.png"
+            alt=""
+            loading="lazy"
+          />
+        </picture>
+        <div class="HeroBlock__summary">
+           finnur þú allar umsóknir borgarinnar. Sumar eru rafrænar, en aðrar á eyðublöðum. Eyðublöð má fylla út og senda með tölvupósti eða prenta út og skila á pappír.
+        </div>
+        <div class="HeroBlock__buttons">
+          <a class="ButtonTertiary" href="/">Um mig</a>
+        </div>
+      </div>
+
+      <div class="TableWrapper TableWrapper--BasicTable">
+        <table class="BasicTable">
+          <thead>
+            <tr>
+              <th class="Cell--number" scope="row">Nr. umsóknar</th>
+              <th>Lýsing</th>
+              <th>Sent dags. / kl</th>
+              <th>Staða</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th class="Cell--number" scope="row"><a href="/">4811029</a></th>
+              <td>Umsókn um skólamáltið / Hagaskóli</td>
+              <td>21.01 2021 - 23:11</td>
+              <td>Samþykkt</td>
+            </tr>
+            <tr>
+              <th class="Cell--number" scope="row"><a href="/">9275521</a></th>
+              <td>Byggingarleyfi</td>
+              <td>03.11 2020 - 11:07</td>
+              <td>Móttekið</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="ExtraLinks">
+        <div class="ExtraLinks__main">
+          <h2 class="ExtraLinks__title">Extra links top</h2>
+          <ul class="ExtraLinks__list">
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+            <li class="ExtraLinks__item">
+              <a class="ExtraLinks__card" href="">
+                <span class="ExtraLinks__card__title">Eignaskiptayfirlýsing</span>
+                <span class="ExtraLinks__card__summary"
+                  >Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss.</span
+                >
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
